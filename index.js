b2e909a16258f1dd459e15b652d0f7463e97350d
Srimped <nickey200202@gmail.com>
2025-11-20 00:04:24 +0700

    feat: driver list
    adding driver list and get detail driver API to server

--- index.js
+++ index.js
@@ -198,6 +198,189 @@ app.delete('/api/user/:id', (req, res) => {
 
 
 
+
+//Driver API
+//GET all driver
+app.get('/api/driver', (req, res) => {
+    res.set('content-type', 'application/json');
+
+    const SQL = 'SELECT id, license_number, vehicle_type, vehicle_brand, vehicle_model, license_plate FROM driver_profiles';
+    
+    let data = { drivers: [] };
+
+    DB.all(SQL, [], (err, rows) => {
+        if (err) {
+            console.error(`Database Error: ${err.message}`);
+            return res.status(500).send(JSON.stringify({ 
+                code: 500, 
+                status: `Database error during query: ${err.message}` 
+            }));
+        }
+
+        for (const rec of rows) {
+            data.drivers.push({
+                id: rec.id,
+                licenseNo: rec.license_number,
+                licensePlate: rec.license_plate,
+                type: rec.vehicle_type,
+                brand: rec.vehicle_brand,
+                model: rec.vehicle_model,
+            });
+        }
+
+        let content = JSON.stringify(data);
+        res.send(content);
+    });
+});
+
+//GET selected driver by id
+app.get('/api/driver/:id', (req, res) => {
+    res.set('content-type', 'application/json');
+    
+    const driverId = req.params.id;
+
+    const SQL = 'SELECT * FROM driver_profiles WHERE id = ?';
+
+    const params = [driverId];
+
+    DB.get(SQL, params, (err, row) => {
+        if (err) {
+            console.error(`Database Error: ${err.message}`);
+            // 500 Internal Server Error for database issues
+            return res.status(500).send(JSON.stringify({ 
+                code: 500, 
+                status: `Database error during query: ${err.message}` 
+            }));
+        }
+
+        if (!row) {
+            return res.status(404).send(JSON.stringify({
+                code: 404,
+                status: `Driver with id ${userId} not found`
+            }));
+        }
+
+        // 4. Format the result and send the successful response (200 OK)
+        let data = {
+            user: {
+                id: row.id,
+                licenseNo: row.license_number,
+                licenseImg: row.license_image,
+                type: row.vehicle_type,
+                brand: row.vehicle_brand,
+                model: row.vehicle_model,
+                year: row.vehicle_year,
+                licensePlate: row.license_plate,
+                available: row.is_available,
+                latitude: row.current_latitude,
+                longitude: row.current_longitude,
+                document: row.document_verified,
+            }
+        };
+
+        let content = JSON.stringify(data);
+        res.status(200).send(content);
+    });
+});
+
+
+//POST
+// app.post('/api/user', (req, res) => {
+
+// })
+//PUT
+// app.put('/api/user/:id', (req, res) => {
+//     res.set('content-type', 'application/json');
+
+//     const {
+//         id,
+//         email,
+//         full_name,
+//         phone,
+//         avatar_url,
+//         is_active,
+//         is_verified,
+//         rating,
+//         total_trips
+//     } = req.body;
+
+//     if (!id) {
+//         return res.status(400).json({ message: "Missing ID in request body" });
+//     }
+
+//     const SQL = `
+//         UPDATE users
+//         SET email = ?,
+//             full_name = ?,
+//             phone = ?,
+//             avatar_url = ?,
+//             is_active = ?,
+//             is_verified = ?,
+//             rating = ?,
+//             total_trips = ?,
+//             updated_at = CURRENT_TIMESTAMP
+//         WHERE id = ?;
+//     `;
+
+//     const params = [
+//         email,
+//         full_name,
+//         phone,
+//         avatar_url,
+//         is_active,
+//         is_verified,
+//         rating,
+//         total_trips,
+//         id
+//     ];
+
+//     DB.run(SQL, params, function (err) {
+//         if (err) {
+//             console.log("DB ERROR:", err);
+//             return res.status(500).json({ message: err.message });
+//         }
+
+//         if (this.changes > 0) {
+//             return res.status(200).json({
+//                 id,
+//                 message: "User data updated successfully"
+//             });
+//         } else {
+//             return res.status(404).json({ message: "User not found" });
+//         }
+//     });
+// });
+
+
+// //DELETE
+// app.delete('/api/user/:id', (req, res) => {
+//     const userId = req.params.id;
+
+//     const SQL = `
+//         DELETE FROM users
+//         WHERE id = ?
+//     `;
+
+//     DB.run(SQL, [userId], function (err) {
+//         if (err) {
+//             console.log("DB ERROR:", err.message);
+//             return res.status(500).json({ message: "Database error: " + err.message });
+//         }
+
+//         if (this.changes > 0) {
+//             return res.status(200).json({ 
+//                 message: "User deleted successfully", 
+//                 deletedId: userId 
+//             });
+//         }
+
+//         return res.status(404).json({ message: "User not found" });
+//     });
+// });
+
+
+
+
 app.listen(5000, err => {
   if(err) {
     console.log('ERROR: ', err.message)
